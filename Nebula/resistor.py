28ac228119860da9904ce8a5d6ac27b18e65eff2
deadpool7777 <85618441+deadpool7777@users.noreply.github.com>
2024-10-21 12:41:45 +0200

    Update resistor.py
    updated power equation

--- Nebula/resistor.py
+++ Nebula/resistor.py
@@ -5,12 +5,8 @@ class Resistor:
         self.resistance = resistance
 
     def calculate_power_dissipation(self, current):
-        """
+        #power equation
         Calculate power dissipation in the resistor using P = I^2 * R
-        Args:
-            current (float): Current flowing through the resistor in Amperes
-        Returns:
-            float: Power dissipation in Watts
-        """
+        
         power = current ** 2 * self.resistance
         return power  
